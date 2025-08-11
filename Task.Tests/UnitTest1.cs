e04d313a12fbeb2d5fea20b225530e4dd74af797
amoorali <amirfallahi222@gmail.com>
2025-08-11 12:34:28 +0330

    final code

---
+++ Task.Tests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿namespace TaskSolution
+{
+    public class UnitTest1
+    {
+        // Helper function to convert integer array to an array of integers.
+        private static List<(int i, int j)> ToPairs(List<int[]> nums)
+            => nums.Select(n => (n[0], n[1])).ToList();
+
+        [Fact]
+        public void SinglePairTest()
+        {
+            var nums = new int[] { 2, 7, 11, 15 };
+            var target = 9;
+            var result = Program.TwoSum(nums, target);
+            var expected = new List<(int i, int j)> { (0, 1) };
+            Assert.Equal(expected, ToPairs(result));
+        }
+
+        [Fact]
+        public void MultiplePairsTest()
+        {
+            var nums = new int[] { 3, 2, 4, 3 };
+            var target = 6;
+            var result = Program.TwoSum(nums, target);
+            var expected = new List<(int i, int j)> { (0, 3), (1, 2)};
+            Assert.Equal(expected, ToPairs(result));
+        }
+    }
+}
---
+++ TaskApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TaskSolution
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Please run the `dotnet test` CLI to check the test cases.");
+        }
+
+        public static List<int[]> TwoSum(int[] nums, int target)
+        {
+            var result = new List<int[]>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var temp = target - nums[i];
+                Console.WriteLine($"{temp}, {target}, {nums[i]}");
+                var n2 = Array.IndexOf(nums, temp, i + 1);
+                if (n2 >= 0) result.Add(new int[] { i, n2 });
+            }
+
+            return result;
+        }
+    }
+}
