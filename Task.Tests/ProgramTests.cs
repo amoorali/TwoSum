16d947fac4221576df5ed11418d70f7005d4840a
amoorali <amirfallahi222@gmail.com>
2025-08-25 19:31:52 +0330

    Added two other methods and test cases.

--- Task.Tests/ProgramTests.cs
+++ Task.Tests/ProgramTests.cs
@@ -7,22 +7,62 @@
             => nums.Select(n => (n[0], n[1])).ToList();
 
         [Fact]
-        public void SinglePairTest()
+        public void SinglePairListTest()
         {
             var nums = new int[] { 2, 7, 11, 15 };
             var target = 9;
-            var result = Program.TwoSum(nums, target);
+            var result = Program.TwoSum_List(nums, target);
             var expected = new List<(int i, int j)> { (0, 1) };
             Assert.Equal(expected, ToPairs(result));
         }
 
         [Fact]
-        public void MultiplePairsTest()
+        public void MultiplePairsListTest()
         {
             var nums = new int[] { 3, 2, 4, 3 };
             var target = 6;
-            var result = Program.TwoSum(nums, target);
+            var result = Program.TwoSum_List(nums, target);
             var expected = new List<(int i, int j)> { (0, 3), (1, 2)};
+            Assert.Equal(expected, ToPairs(result));
+        }
+
+        [Fact]
+        public void SinglePairBruteForceTest()
+        {
+            var nums = new int[] { 2, 7, 11, 15 };
+            var target = 9;
+            var result = Program.TwoSum_BruteForce(nums, target);
+            var expected = new List<(int i, int j)> { (0, 1) };
+            Assert.Equal(expected, ToPairs(result));
+        }
+
+        [Fact]
+        public void MultiplePairsBruteForceTest()
+        {
+            var nums = new int[] { 3, 2, 4, 3 };
+            var target = 6;
+            var result = Program.TwoSum_BruteForce(nums, target);
+            var expected = new List<(int i, int j)> { (0, 3), (1, 2)};
+            Assert.Equal(expected, ToPairs(result));
+        }
+
+        [Fact]
+        public void SinglePairDictionaryTest()
+        {
+            var nums = new int[] { 2, 7, 11, 15 };
+            var target = 9;
+            var result = Program.TwoSum_Dictionary(nums, target);
+            var expected = new List<(int i, int j)> { (0, 1) };
+            Assert.Equal(expected, ToPairs(result));
+        }
+
+        [Fact]
+        public void MultiplePairsDictionaryTest()
+        {
+            var nums = new int[] { 3, 2, 4, 3 };
+            var target = 6;
+            var result = Program.TwoSum_Dictionary(nums, target);
+            var expected = new List<(int i, int j)> { (1, 2), (0, 3) };
             Assert.Equal(expected, ToPairs(result));
         }
     }
--- TaskApp/Program.cs
+++ TaskApp/Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("Please run the `dotnet test` CLI to check the test cases.");
         }
 
-        public static List<int[]> TwoSum(int[] nums, int target)
+        public static List<int[]> TwoSum_List(int[] nums, int target)
         {
             var result = new List<int[]>();
             for (int i = 0; i < nums.Length; i++)
@@ -18,6 +18,35 @@
                 if (n2 >= 0) result.Add(new int[] { i, n2 });
             }
 
+            return result;
+        }
+
+        public static List<int[]> TwoSum_BruteForce(int[] nums, int target)
+        {
+            var result = new List<int[]>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        result.Add(new int[] { i, j });
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static List<int[]> TwoSum_Dictionary(int[] nums, int target)
+        {
+            var result = new List<int[]>();
+            var dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var temp = target - nums[i];
+                if (dict.ContainsKey(temp)) result.Add(new int[] { dict[temp], i });
+                if (!dict.ContainsKey(nums[i])) dict.Add(nums[i], i);
+            }
             return result;
         }
     }
